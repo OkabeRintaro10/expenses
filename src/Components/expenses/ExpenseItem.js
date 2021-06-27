ae8bc4be6541a97ee2f90416656c16fab6476d31
OkabeRintaro10 <kausikremella@gmail.com>
2021-06-27 13:08:20 +0530

    useState

--- src/Components/expenses/ExpenseItem.js
+++ src/Components/expenses/ExpenseItem.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle("Updated");
+  };
+
+  console.log(title);
+
   return (
     <Card className="expense-item">
       <ExpDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
+        <button onClick={clickHandler}>Chage title</button>
       </div>
       <div className="expense-item__price">
         <h2>${props.amount}</h2>
